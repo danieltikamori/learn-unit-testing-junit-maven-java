50bb843229e6ba9a349674bea26da50fa6b92e54
Daniel Tikamori <tikamori.itiro@gmail.com>
2024-03-22 22:23:29 +0900

    Initial commit

---
+++ src/test/java/AccountTest.java
@@ -0,0 +1,8 @@
+/*
+ * Copyright (c) 2024 Daniel I. Tikamori. All rights reserved.
+ */
+
+import static org.junit.jupiter.api.Assertions.*;
+class AccountTest {
+  
+}
---
+++ src/test/java/PersonTest.java
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2024 Daniel I. Tikamori. All rights reserved.
+ */
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest {
+
+}
